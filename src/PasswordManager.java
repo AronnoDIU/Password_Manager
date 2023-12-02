a5f419d5b769e00a1ba1164c6dd0bfd1678f2772
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-03 04:29:42 +0600

    Password Manager

--- src/PasswordManager.java
+++ src/PasswordManager.java
@@ -1,7 +1,7 @@
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.MessageDigest; // For hashing the password using SHA-256
+import java.security.NoSuchAlgorithmException; // For hashing the password using SHA-256
+import java.util.HashMap; // For storing the account name and hashed password
+import java.util.Map; // For storing the account name and hashed password
 import java.util.Scanner;
 
 public class PasswordManager {
@@ -20,22 +20,23 @@ public class PasswordManager {
             scanner.nextLine(); // Consume newline
 
             switch (choice) {
-                case 1:
+                case 1: // For Store password
                     storePassword();
                     break;
-                case 2:
+                case 2: // For Retrieve password
                     retrievePassword();
                     break;
-                case 3:
+                case 3: // For Exit
                     System.out.println("Exiting Password Manager. Goodbye!");
                     System.exit(0);
                     break;
-                default:
+                default: // For invalid choice
                     System.out.println("Invalid choice. Please try again.");
             }
         }
     }
 
+    // Store the account name and hashed password in the password vault
     private static void storePassword() {
         System.out.print("Enter the account name: ");
         String account = scanner.nextLine();
@@ -44,17 +45,20 @@ public class PasswordManager {
         String password = scanner.nextLine();
 
         // Hash the password before storing it
-        String hashedPassword = hashPassword(password);
+        String hashedPassword = hashPassword(password); // Hash the password using SHA-256
 
+        // Store the account name and hashed password in the password vault
         passwordVault.put(account, hashedPassword);
 
         System.out.println("Password stored successfully for account: " + account);
     }
 
+    // Retrieve the password from the password vault and verify it against the entered password
     private static void retrievePassword() {
         System.out.print("Enter the account name: ");
         String account = scanner.nextLine();
 
+        // Check if the account exists in the password vault
         if (passwordVault.containsKey(account)) {
             System.out.print("Enter the password: ");
             String enteredPassword = scanner.nextLine();
@@ -63,6 +67,7 @@ public class PasswordManager {
             String storedHashedPassword = passwordVault.get(account);
             String enteredHashedPassword = hashPassword(enteredPassword);
 
+            // Check if the entered password matches the stored hashed password
             if (storedHashedPassword.equals(enteredHashedPassword)) {
                 System.out.println("Password retrieved successfully for account: " + account);
             } else {
@@ -73,6 +78,7 @@ public class PasswordManager {
         }
     }
 
+    // Hash the password using SHA-256 and return the hashed password
     private static String hashPassword(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -84,7 +90,7 @@ public class PasswordManager {
                 hexStringBuilder.append(String.format("%02x", b));
             }
 
-            return hexStringBuilder.toString();
+            return hexStringBuilder.toString(); // Return the hashed password
         } catch (NoSuchAlgorithmException e) {
             // Handle the exception appropriately
             e.printStackTrace();
